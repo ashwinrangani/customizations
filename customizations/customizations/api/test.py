5f39111e5182205ebe199b8adb1b5c55b0fa62b8
ashwin rangani <ashwyn.rangani@gmail.com>
2024-12-02 22:39:22 +0530

    scan-doc-api

---
+++ customizations/customizations/api/test.py
@@ -0,0 +1,14 @@
+class MyClass:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        while True:
+            z = input('Enter Your Name:')
+            print()
+       
+    
+    def other(self):
+        print('other')
+
+
+MyClass(12,13)
